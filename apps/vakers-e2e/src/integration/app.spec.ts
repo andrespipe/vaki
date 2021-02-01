7f2ed7d116f1442231076149a7b06324e21e9148
andresm <andresm@blanclabs.com>
2021-02-01 17:23:54 -0500

    Cypress

--- apps/vakers-e2e/src/integration/app.spec.ts
+++ apps/vakers-e2e/src/integration/app.spec.ts
@@ -1,13 +1,54 @@
 import { getGreeting } from '../support/app.po';
 
 describe('vakers', () => {
-  beforeEach(() => cy.visit('/'));
+  const vakiHomeUrl = '/';
+  const vakiTestUrl = 'vaki/estoy-con-bahia-solano';
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('Open home ', () => {
+    cy.visit(vakiHomeUrl);
+    const homeSelector = 'vaki-challenge-home';
+    const homeObj = cy.get(homeSelector);
+    homeObj.should('be.visible');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to vakers!');
+  it('Should open vaki', () => {
+    const vakiBtnSelector =
+      'vaki-challenge-home > div > mat-card:nth-child(1) > mat-card-actions > a';
+    const vakiBtn = cy.get(vakiBtnSelector);
+    vakiBtn.click();
+    cy.wait(5000);
+    const vakiSelector = '.vaki-photo';
+    cy.get(vakiSelector);
+  });
+
+  it('Should add to cart', () => {
+    cy.visit(vakiTestUrl);
+    const addCartSelector = 'mat-card:nth-child(1) > mat-card-actions > button';
+    const addCartBtn = cy.get(addCartSelector);
+    addCartBtn.click();
+
+    const shoppingCartBtnSelector =
+      'vaki-challenge-shopping-cart > div > button > span.mat-button-wrapper > mat-icon';
+    cy.get(shoppingCartBtnSelector);
+  });
+
+  it('Should remove from cart', () => {
+    cy.visit(vakiTestUrl);
+    const addCartSelector = 'mat-card:nth-child(1) > mat-card-actions > button';
+    const addCartBtn = cy.get(addCartSelector);
+    addCartBtn.click();
+    cy.screenshot('removedFromCartAdded');
+
+    const shoppingCartBtnSelector =
+      'vaki-challenge-shopping-cart > div > button > span.mat-button-wrapper > mat-icon';
+    const shoppingCartBtn = cy.get(shoppingCartBtnSelector);
+    shoppingCartBtn.click();
+    cy.screenshot('removedFromCartShowCart');
+
+    const removeBtnSelector =
+      'vaki-challenge-shopping-cart > mat-card > mat-card-content > mat-list > mat-list-item > div > button';
+    const removeBtn = cy.get(removeBtnSelector);
+    removeBtn.click();
+    cy.screenshot('removedFromCartEnd');
   });
 });
